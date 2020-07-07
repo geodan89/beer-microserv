ac3609c2e6bebea2cf169557f1e6e041cb5a2ee9
geodan89 <geodan89@gmail.com>
2020-07-07 23:25:31 +0300

    intial project created. closes #1

---
+++ src/main/java/com/beerproject/beermicroserv/BeerMicroservApplication.java
@@ -0,0 +1,13 @@
+package com.beerproject.beermicroserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerMicroservApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerMicroservApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beerproject/beermicroserv/BeerMicroservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beerproject.beermicroserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerMicroservApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
