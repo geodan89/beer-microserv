c2f4702321156d9efb08b89409dcb3530ad4028d
geodan89 <geodan89@gmail.com>
2020-07-16 20:33:31 +0300

    Added Spring Data JPA Repository for Beer object, closes #6

---
+++ src/main/java/com/beerproject/beermicroserv/repository/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.beerproject.beermicroserv.repository;
+
+import com.beerproject.beermicroserv.domain.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
+}
